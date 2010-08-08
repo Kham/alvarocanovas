e694a0dcbc0e2de95e949cee26b31e14987bcad6
= <nicolas.papon@webflows.fr>
2010-08-08 17:29:32 +0200

    css + js des category_pictures

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,9 +1,29 @@
 $(function(){
 
+  // Initialisation menu principal
   $('#menu').mainMenu();
+
+  // Accès direct a une gallery depuis la page categories/:id/show
   $('.category_pictures').live('click',function(){
     $('#content').clearQueue();
   });
+
+  // Initialization du sous menu des categories
+  $('.category.menu').livequery(
+    function(){
+      $(this).find('ul').css('display','none');
+      $(this).hover(
+        function(){
+          $(this).children('ul').slideDown();
+        },
+        function(){
+          $(this).children('ul').slideUp();
+        }
+      );
+    }
+  );
+
+  // Initilisation des galleries
   $("#thumbs").livequery(function(){
     $(this).gallery('classic');
   });
