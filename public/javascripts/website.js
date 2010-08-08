4e3bc5e38a0d7d87fb560976545329ac2d19df22
= <nicolas.papon@webflows.fr>
2010-08-08 17:06:38 +0200

    css & html show category

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,7 +1,9 @@
 $(function(){
 
   $('#menu').mainMenu();
-
+  $('.category_pictures').live('click',function(){
+    $('#content').clearQueue();
+  });
   $("#thumbs").livequery(function(){
     $(this).gallery('classic');
   });
