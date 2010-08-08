c6a3b43f1d787c9b484305ef68c187ecdcad9cb2
= <nicolas.papon@webflows.fr>
2010-08-08 19:26:55 +0200

    refactorign js main menu

--- public/javascripts/website.js
+++ public/javascripts/website.js
@@ -1,7 +1,37 @@
 $(function(){
 
   // Initialisation menu principal
-  $('#menu').mainMenu();
+//  $('#menu').mainMenu();
+  $('#menu ul').hide();
+
+  $('#menu').hover(
+    function(){
+      $(this)
+        .find('h2').animate({top: "55px"})
+        .find('span').fadeOut(100);
+      $(this)
+        .find('>ul')
+        .fadeIn()
+    },
+    function(){
+      $(this)
+        .find('h2').animate({top: "20px"})
+        .find('span').fadeIn(100);
+      $(this)
+        .find('>ul')
+        .fadeOut()
+    }
+  );
+
+  // Sous menus du menu principal
+  $("#menu li").hover(
+    function(){
+      $(this).find('>ul').fadeIn('fast')
+    },
+    function(){
+      $(this).find('>ul').fadeOut('fast')
+    }
+  );
 
   // Accès direct a une gallery depuis la page categories/:id/show
   $('.category_pictures').live('click',function(){
